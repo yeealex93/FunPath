837c570948affca3460d83ca001b6fca81498752
yeealex <yeealex93@gmail.com>
2014-12-13 02:57:08 -0500

    added a login.txt file under files directory. Also set up but did not test the file reading and writing. Also need to finish the home page to display their username

--- src/cmsc434/funpath/login/LoginActivity.java
+++ src/cmsc434/funpath/login/LoginActivity.java
@@ -1,8 +1,19 @@
 package cmsc434.funpath.login;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -10,10 +21,18 @@ import cmsc434.funpath.R;
 
 public class LoginActivity extends Activity {
 
+	public static final String LOGIN_FILEPATH = Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + "files";
+	public static final String LOGIN_FILENAME = "login.txt"; //Change if textfile name changes!
+	
+	
+	public static final HashMap<String, String> loginMap = new HashMap<String, String>();
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+		
+		loadLoginData();
 		
 		Button newUserButton = (Button) findViewById(R.id.new_user_register_button);
 		newUserButton.setOnClickListener(new OnClickListener(){
@@ -29,18 +48,48 @@ public class LoginActivity extends Activity {
 			
 		});
 		
-		Button loginButton = (Button) findViewById(R.id.new_user_register_button);
+		Button loginButton = (Button) findViewById(R.id.login_button);
 		loginButton.setOnClickListener(new OnClickListener(){
 
 			@Override
 			public void onClick(View v) {
-				// TODO just redirects to RegisterActivity for now
-				// can set up to startActivityForResult and auto-fill in their username/password
-				// when returned
+				
+				//TODO check credentials!
+				
 				Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
 				startActivity(intent);
 			}
 			
 		});
 	}
+
+	// Read in login data from login.txt and populate loginMap with username/password pairs
+	private static void loadLoginData() {
+		File file = new File(LOGIN_FILEPATH, LOGIN_FILENAME);
+		
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			String [] loginStr;
+		    String line = br.readLine();
+		    
+		    while (line != null) {
+		        loginStr = line.split("\t");
+		        loginMap.put(loginStr[0], loginStr[1]);
+		        line = br.readLine();
+		    }
+		    br.close();
+		} catch (IOException e) {
+			//TODO handle error... not sure what to do honestly. 
+			//App should probably crash if it can't read login data...
+			//Doing nothing for now.
+			Log.i("FunPath", "IO EXCEPTION while reading login.txt.");
+		} catch (Exception e) {
+			//in case there's a typo/spacing error in text file and a NullPointerException is thrown
+			//or other non-IO error...
+			//Doing nothing again -- so playful!
+			Log.i("FunPath", "Non-IO error while reading login.txt.");
+		}
+	}
+	
+
 }
--- src/cmsc434/funpath/login/RegisterActivity.java
+++ src/cmsc434/funpath/login/RegisterActivity.java
@@ -1,11 +1,31 @@
 package cmsc434.funpath.login;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 import cmsc434.funpath.R;
 
 public class RegisterActivity extends Activity {
 
+	public static String USERNAME = "";
+	
+	//TODO Are these error messages correct for design principles?
+	private	final static String PASSWORDS_DONT_MATCH = "Passwords don't match!";
+	private	final static String USERNAME_BLANK = "Please enter a username.";
+	private	final static String DUPLICATE_USERNAME = "Username already exists.";
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -14,5 +34,81 @@ public class RegisterActivity extends Activity {
 		//TODO after a success registration, should have either an activity or textual confirmation
 		//saying "registration successful" or something to that effect.
 		
+		final EditText usernameView = (EditText) findViewById(R.id.register_username);
+		final EditText passwordView = (EditText) findViewById(R.id.register_password);
+		final EditText passwordConfirmView = (EditText) findViewById(R.id.register_password_confirm);
+		
+		Button returnButton = (Button) findViewById(R.id.return_to_login_button);
+		returnButton.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+				startActivity(intent);
+				//If we assume this is only called from LoginActivitiy, I think "finish();" may suffice...
+			}
+			
+		});
+		
+		Button registerButton = (Button) findViewById(R.id.register_button);
+		registerButton.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				//TODO error checking
+				String username = usernameView.getText().toString();
+				if (LoginActivity.loginMap.containsKey(username)) {
+					
+					//if the username already exists
+					Toast.makeText(RegisterActivity.this, DUPLICATE_USERNAME, Toast.LENGTH_LONG).show();
+					
+				} else if (username != "" ){
+					
+					//if username is empty
+					Toast.makeText(RegisterActivity.this, USERNAME_BLANK, Toast.LENGTH_LONG).show();
+					
+				} else if (passwordView.getText().toString().equals(passwordConfirmView.getText().toString())) {
+					
+					//if passwords are not the same
+					Toast.makeText(RegisterActivity.this, PASSWORDS_DONT_MATCH, Toast.LENGTH_LONG).show();
+					
+				} else {
+					
+					//successful registration
+					
+					//1. set global USERNAME variable (indicating the logged in user)
+					//2. update loginMap 
+					//3. update login.txt file
+					
+					USERNAME = username;
+					LoginActivity.loginMap.put(username, passwordView.getText().toString());
+					
+					
+					Intent intent = new Intent(RegisterActivity.this, HomeActivity.class);
+					startActivity(intent);
+				}
+			}
+			
+		});
+	}
+	
+	// Write newUsername/newPassword pair to login.txt
+	public void updateLoginData(String newUsername, String newPassword) {
+		File file = new File(LoginActivity.LOGIN_FILEPATH, LoginActivity.LOGIN_FILENAME);
+		
+		try {
+			//TODO: test which of these is correct:
+			FileOutputStream outputStream = openFileOutput(file.getAbsolutePath(), Context.MODE_APPEND);
+			//FileOutputStream outputStream = openFileOutput(LoginActivity.LOGIN_FILENAME, Context.MODE_APPEND);
+			
+			final String toWrite = "\n" + newUsername + "\t" + newPassword;
+			outputStream.write(toWrite.getBytes());
+			
+			outputStream.flush(); //may not be necessary
+			outputStream.close();
+		} catch (IOException e) {
+			//TODO handle error...
+			Log.i("FunPath", "Error updating login.txt with new registered user.");
+		}
 	}
 }
