7af3fcd9f01a20822d87f14526466b91ca269e05
yeealex <yeealex93@gmail.com>
2014-12-14 19:16:31 -0500

    was missing an import on registerActivity

--- src/cmsc434/funpath/login/RegisterActivity.java
+++ src/cmsc434/funpath/login/RegisterActivity.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
